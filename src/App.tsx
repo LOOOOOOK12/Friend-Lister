2935729d529080fa91ea0477f3ad05d643ecd15d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-05 22:05:06 +0800

    text added and div for containers

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      
+    <div className='h-screen flex flex-col justify-center items-center'>
+      <div className='flex flex-col items-center'>
+        <h1>Friend Lister</h1>
+        <h2>Add your friends and see the details</h2>
+      </div>
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+
+      </div>
     </div>
   )
 }
