0c677b71b2977656b0866e6b6d7ade1273368a11
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-06 18:24:59 +0800

    test button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <div className='flex flex-col items-center'>
         <h1>Friend Lister</h1>
         <h2>Add your friends and see the details</h2>
+        <Button>Add New Friend</Button>
       </div>
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
 
