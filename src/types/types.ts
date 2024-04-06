049986c1bed85a43d8a55d5e1bc082927471e7f6
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-06 18:25:55 +0800

    added on click props

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,10 +2,12 @@
 export type Friends = {
     image: string;
     name: string;
+    age: number;
     gender: string
 }
 
 //Functions
 export type Add = {
     buttonName: string;
+    onclick: () => void; 
 }
