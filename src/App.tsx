91376feda573939fbf95c233fadd675dd726dfed
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-07 15:56:43 +0800

    add friend modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
-import { Button } from "@/components/ui/button"
+import AddFriend from './modals/AddFriend'
 
 function App() {
+
   return (
     <div className='h-screen flex flex-col justify-center items-center'>
       <div className='flex flex-col items-center'>
         <h1>Friend Lister</h1>
         <h2>Add your friends and see the details</h2>
-        <Button>Add New Friend</Button>
+        <AddFriend/>
       </div>
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-
       </div>
     </div>
   )
