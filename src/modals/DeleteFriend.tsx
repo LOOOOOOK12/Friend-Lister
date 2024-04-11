c0816ab30a239a2538afc89eec82831b1ca29033
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-11 19:27:32 +0800

    delete Friend modal added

---
+++ src/modals/DeleteFriend.tsx
@@ -0,0 +1,31 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { Button } from "@/components/ui/button"
+
+function DeleteFriend() {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline">Delete</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px] flex flex-col items-center">
+                <DialogHeader>
+                    <DialogTitle>Are you sure to delete your friend?</DialogTitle>
+                </DialogHeader>
+                <DialogFooter className="flex items-center justify-center">
+                    <Button type="submit">Yes</Button>
+                    <Button>No</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default DeleteFriend
