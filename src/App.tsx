ba9a727cf3112e30b450d1f5bb22c8827b5572d8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-11 19:26:24 +0800

    Test Map of containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css'
 import AddFriend from './modals/AddFriend'
+import FriendContainer from './components/containers/friendContainer'
 
 function App() {
 
   return (
-    <div className='h-screen flex flex-col justify-center items-center'>
-      <div className='flex flex-col items-center'>
+    <div className='py-8 flex flex-col gap-10 justify-center items-center '>
+      <div className='flex flex-col gap-5 items-center'>
         <h1>Friend Lister</h1>
         <h2>Add your friends and see the details</h2>
         <AddFriend/>
       </div>
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        <FriendContainer/>
+        <FriendContainer/>
+        <FriendContainer/>  
       </div>
     </div>
   )
