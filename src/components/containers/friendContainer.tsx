27b69abfdd87d6950777effd9c58f5219577c37b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-11 19:26:50 +0800

    friend container Added

---
+++ src/components/containers/friendContainer.tsx
@@ -0,0 +1,32 @@
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import DeleteFriend from "@/modals/DeleteFriend"
+import EditFriend from "@/modals/EditFriend"
+
+function friendContainer() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Michael Jackstone</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col justify-center items-center">
+                <img src="src\assets\sampleimages\js.png" alt="" className="h-32" />
+                <h1>20 Years Old</h1>
+                <h1>Febuary 29 2024</h1>
+                <h1>Bading</h1>
+            </CardContent>
+            <CardFooter className="gap-3 justify-end">
+                <DeleteFriend/>
+                <EditFriend/>
+            </CardFooter>
+        </Card>
+
+    )
+}
+
+export default friendContainer
