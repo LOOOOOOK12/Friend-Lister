d1a0fb9595744b9d73b138a8ebe82585c849b395
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-11 19:27:19 +0800

    Birthday input Added

--- src/modals/AddFriend.tsx
+++ src/modals/AddFriend.tsx
@@ -20,24 +20,24 @@ function AddFriend() {
             <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
                 <DialogTitle>Add your Friend</DialogTitle>
-                <DialogDescription>
-                    Enter your details about your friend!!
-                </DialogDescription>
+                    <DialogDescription>
+                        Enter your details about your friend!!
+                    </DialogDescription>
                 </DialogHeader>
                 <div className="flex flex-col gap-5">
                     <div className="flex flex-col gap-2">
                         <Label htmlFor="name">
-                        Name
+                            Name
                         </Label>
                         <Input
-                        id="name"
-                        defaultValue="Juan Dela Cruz"
-                        className="col-span-3"
+                            id="name"
+                            defaultValue="Juan Dela Cruz"
+                            className="col-span-3"
                         />
                     </div>
                     <div className="flex flex-col gap-2">
                         <Label htmlFor="age">
-                        Age
+                            Age
                         </Label>
                         <Input
                         id="age"
@@ -46,12 +46,23 @@ function AddFriend() {
                         />
                     </div>
                     <div className="flex flex-col gap-2">
+                        <Label htmlFor="birthday">
+                            Birthday
+                        </Label>
+                        <Input
+                            id="birthday"
+                            defaultValue="@peduarte"
+                            type="date"
+                            className="col-span-3"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-2">
                         <Label htmlFor="picture">Picture</Label>
                         <Input id="picture" type="file" />
                     </div>
                 </div>
                 <DialogFooter>
-                <Button type="submit">Save changes</Button>
+                    <Button type="submit">Add your Friend!</Button>
                 </DialogFooter>
             </DialogContent>
     </Dialog>
