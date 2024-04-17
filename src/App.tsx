20d201fffbd2d37c0824fae079f062ddb9b7472e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-17 23:06:40 +0800

    added sample data

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,15 @@ function App() {
   return (
     <div className='py-8 flex flex-col gap-10 justify-center items-center '>
       <div className='flex flex-col gap-5 items-center'>
-        <h1>Friend Lister</h1>
-        <h2>Add your friends and see the details</h2>
+        <h1 className='text-5xl font-semibold'>Friend Lister</h1>
+        <h2 className='text-2xl'>Add your friends and see the details</h2>
         <AddFriend/>
       </div>
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
         <FriendContainer/>
         <FriendContainer/>
-        <FriendContainer/>  
+        <FriendContainer/>
+        <FriendContainer/>    
       </div>
     </div>
   )
