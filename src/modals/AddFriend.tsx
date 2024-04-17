776825f159f404b251444909ebea712199df3d22
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-17 23:07:38 +0800

    added label components

--- src/modals/AddFriend.tsx
+++ src/modals/AddFriend.tsx
@@ -7,9 +7,8 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import Labels from "@/components/containers/labels"
 
 function AddFriend() {
     return (
@@ -24,43 +23,27 @@ function AddFriend() {
                         Enter your details about your friend!!
                     </DialogDescription>
                 </DialogHeader>
-                <div className="flex flex-col gap-5">
-                    <div className="flex flex-col gap-2">
-                        <Label htmlFor="name">
-                            Name
-                        </Label>
-                        <Input
-                            id="name"
+                    <div className="flex flex-col gap-5">
+                        <Labels
+                            labelName="Name"
+                            type="text"
                             defaultValue="Juan Dela Cruz"
-                            className="col-span-3"
                         />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <Label htmlFor="age">
-                            Age
-                        </Label>
-                        <Input
-                        id="age"
-                        defaultValue="@peduarte"
-                        className="col-span-3"
+                        <Labels
+                            labelName="Age"
+                            type="text"
+                            defaultValue="12"
                         />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <Label htmlFor="birthday">
-                            Birthday
-                        </Label>
-                        <Input
-                            id="birthday"
-                            defaultValue="@peduarte"
+                        <Labels
+                            labelName="Birthday"
                             type="date"
-                            className="col-span-3"
+                            defaultValue="12"
+                        />
+                        <Labels
+                            labelName="Picture"
+                            type="file"
                         />
                     </div>
-                    <div className="flex flex-col gap-2">
-                        <Label htmlFor="picture">Picture</Label>
-                        <Input id="picture" type="file" />
-                    </div>
-                </div>
                 <DialogFooter>
                     <Button type="submit">Add your Friend!</Button>
                 </DialogFooter>
