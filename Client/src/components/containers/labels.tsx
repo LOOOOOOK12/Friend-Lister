039fc0183c85d723a569811f7c105574b46909ae
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-18 18:44:14 +0800

    added some extra types, handlechange, and values

--- Client/src/components/containers/labels.tsx
+++ Client/src/components/containers/labels.tsx
@@ -1,24 +1,35 @@
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
+import React from 'react';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 
-interface Props{
+interface Props {
     labelName: string;
     type: string;
-    defaultValue?: string;
+    placeholder?: string;
+    value: string | number;
+    onChange?: (value: string) => void;
 }
 
-function labels({labelName, type, defaultValue}:Props) {
+function Labels({ labelName, type, placeholder, value, onChange }: Props) {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (onChange) {
+        onChange(event.target.value);
+        }
+    };
+
     return (
         <div className="flex flex-col gap-2">
-            <Label>{labelName}</Label>
-            <Input
-                id="name"
-                defaultValue={defaultValue}
-                type={type}
-                className="col-span-3"
-            />
+        <Label>{labelName}</Label>
+        <Input
+            id={labelName.toLowerCase()}
+            placeholder={placeholder}
+            type={type}
+            onChange={handleChange}
+            value={value.toString()} 
+            className="col-span-3"
+        />
         </div>
-    )
+    );
 }
 
-export default labels
+export default Labels;
