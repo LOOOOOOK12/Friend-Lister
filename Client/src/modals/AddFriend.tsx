c07f9a2db02e527e9a5eb97ebdfeb45dc5c744a5
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-18 18:45:02 +0800

    added the value and onchange function in the labels

--- Client/src/modals/AddFriend.tsx
+++ Client/src/modals/AddFriend.tsx
@@ -1,55 +1,73 @@
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import { Button } from "@/components/ui/button"
-import Labels from "@/components/containers/labels"
+import React, { useState } from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import Labels from '@/components/containers/labels';
 
 function AddFriend() {
+    const [friend, setFriend] = useState({
+        name: '',
+        age: '',
+        birthday: '',
+        picture: '',
+    });
+
+    const handleChange = (key: string, value: string) => {
+        setFriend({
+        ...friend,
+        [key]: value,
+        });
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // Add logic to handle form submission with friend data
+    };
+
     return (
         <Dialog>
-            <DialogTrigger asChild>
-                <Button variant="outline">Add Friend</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                <DialogTitle>Add your Friend</DialogTitle>
-                    <DialogDescription>
-                        Enter your details about your friend!!
-                    </DialogDescription>
-                </DialogHeader>
-                    <div className="flex flex-col gap-5">
-                        <Labels
-                            labelName="Name"
-                            type="text"
-                            defaultValue="Juan Dela Cruz"
-                        />
-                        <Labels
-                            labelName="Age"
-                            type="text"
-                            defaultValue="12"
-                        />
-                        <Labels
-                            labelName="Birthday"
-                            type="date"
-                            defaultValue="12"
-                        />
-                        <Labels
-                            labelName="Picture"
-                            type="file"
-                        />
-                    </div>
-                <DialogFooter>
-                    <Button type="submit">Add your Friend!</Button>
-                </DialogFooter>
-            </DialogContent>
-    </Dialog>
-    )
+        <DialogTrigger asChild>
+            <Button variant="outline">Add Friend</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+            <DialogTitle>Add your Friend</DialogTitle>
+            <DialogDescription>Enter your details about your friend!!</DialogDescription>
+            </DialogHeader>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+            <Labels
+                labelName="Name"
+                type="text"
+                placeholder="Juan Dela Cruz"
+                value={friend.name}
+                onChange={(value) => handleChange('name', value)}
+            />
+            <Labels
+                labelName="Age"
+                type="text"
+                placeholder="12"
+                value={friend.age}
+                onChange={(value) => handleChange('age', value)}
+            />
+            <Labels
+                labelName="Birthday"
+                type="date"
+                placeholder="12"
+                value={friend.birthday}
+                onChange={(value) => handleChange('birthday', value)}
+            />
+            <Labels
+                labelName="Picture"
+                type="file"
+                value={friend.picture}
+                onChange={(value) => handleChange('picture', value)}
+            />
+            <DialogFooter>
+                <Button type="submit">Add your Friend!</Button>
+            </DialogFooter>
+            </form>
+        </DialogContent>
+        </Dialog>
+    );
 }
 
-export default AddFriend
+export default AddFriend;
