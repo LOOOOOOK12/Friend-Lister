c03a428b76ecf0f212c58984325390abd80c8eb1
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-19 21:56:37 +0800

    card color changes

--- Client/src/components/ui/card.tsx
+++ Client/src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border-2 border-orange-300 bg-transparent text-card-foreground shadow-sm",
       className
     )}
     {...props}
