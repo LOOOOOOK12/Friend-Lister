e3dec7a9c095d1718b25baf43771e58ddf2642ca
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-19 21:56:28 +0800

    background color added

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,7 +5,7 @@ import FriendContainer from './components/containers/friendContainer'
 function App() {
 
   return (
-    <div className='py-8 flex flex-col gap-10 justify-center items-center '>
+    <div className='bg-gradient-to-r from-yellow-300 to-orange-500 py-8 flex flex-col gap-10 justify-center items-center '>
       <div className='flex flex-col gap-5 items-center'>
         <h1 className='text-5xl font-semibold'>Friend Lister</h1>
         <h2 className='text-2xl'>Add your friends and see the details</h2>
