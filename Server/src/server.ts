ab627b8114ca7f3812228642db95675b8771e374
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-22 19:14:45 +0800

    add

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,13 +1,21 @@
+import "dotenv/config"
+import mongoose from 'mongoose'
 import express from 'express'
-
 const app = express()
-
-const port = 5000
 
 app.get("/" ,(req,res) => {
     res.send("Hello")
 })
 
-app.listen(port, () => {
-    console.log("Server started in port" + port)
-})
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+    .then(() => {
+        console.log("Mongoose Connected")
+        app.listen(port, () => {
+            console.log("server running in port " + port)
+        })
+    })
+
+
+
