487c5e0802a4ab65a1f4060bf882a05e75186574
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-23 22:18:45 +0800

    bg color and grid changes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,13 +5,13 @@ import FriendContainer from './components/containers/friendContainer'
 function App() {
 
   return (
-    <div className='bg-gradient-to-r from-yellow-300 to-orange-500 py-8 flex flex-col gap-10 justify-center items-center '>
+    <div className='bg-gradient-to-r from-blue-950 to-violet-900 py-8 flex flex-col gap-10 justify-center items-center '>
       <div className='flex flex-col gap-5 items-center'>
         <h1 className='text-5xl font-semibold'>Friend Lister</h1>
         <h2 className='text-2xl'>Add your friends and see the details</h2>
         <AddFriend/>
       </div>
-      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5'>
         <FriendContainer/>
         <FriendContainer/>
         <FriendContainer/>
