90f8e8634bfb6385582423379549d65d027f2fc1
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-27 18:16:23 +0800

    changes

--- Client/src/modals/AddFriend.tsx
+++ Client/src/modals/AddFriend.tsx
@@ -25,47 +25,47 @@ function AddFriend() {
 
     return (
         <Dialog>
-        <DialogTrigger asChild>
-            <Button className='bg-indigo-800 hover:bg-violet-600'>Add Friend</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-            <DialogTitle>Add your Friend</DialogTitle>
-            <DialogDescription>Enter your details about your friend!!</DialogDescription>
-            </DialogHeader>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-            <Labels
-                labelName="Name"
-                type="text"
-                placeholder="Juan Dela Cruz"
-                value={friend.name}
-                onChange={(value) => handleChange('name', value)}
-            />
-            <Labels
-                labelName="Age"
-                type="text"
-                placeholder="12"
-                value={friend.age}
-                onChange={(value) => handleChange('age', value)}
-            />
-            <Labels
-                labelName="Birthday"
-                type="date"
-                placeholder="12"
-                value={friend.birthday}
-                onChange={(value) => handleChange('birthday', value)}
-            />
-            <Labels
-                labelName="Picture"
-                type="file"
-                value={friend.picture}
-                onChange={(value) => handleChange('picture', value)}
-            />
-            <DialogFooter>
-                <Button type="submit">Add your Friend!</Button>
-            </DialogFooter>
-            </form>
-        </DialogContent>
+            <DialogTrigger asChild>
+                <Button className='bg-indigo-800 hover:bg-violet-600'>Add Friend</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Add your Friend</DialogTitle>
+                    <DialogDescription>Enter your details about your friend!!</DialogDescription>
+                </DialogHeader>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-5 ">
+                    <Labels
+                        labelName="Name"
+                        type="text"
+                        placeholder="Juan Dela Cruz"
+                        value={friend.name}
+                        onChange={(value) => handleChange('name', value)}
+                    />
+                    <Labels
+                        labelName="Age"
+                        type="text"
+                        placeholder="12"
+                        value={friend.age}
+                        onChange={(value) => handleChange('age', value)}
+                    />
+                    <Labels
+                        labelName="Birthday"
+                        type="date"
+                        placeholder="12"
+                        value={friend.birthday}
+                        onChange={(value) => handleChange('birthday', value)}
+                    />
+                    <Labels
+                        labelName="Picture"
+                        type="file"
+                        value={friend.picture}
+                        onChange={(value) => handleChange('picture', value)}
+                    />
+                    <DialogFooter>
+                        <Button type="submit">Add your Friend!</Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
         </Dialog>
     );
 }
