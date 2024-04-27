e59a5e59a1f462fd3bbc093e2029cd12fbd1003d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-27 18:13:57 +0800

    added main tag

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,22 +1,24 @@
 import './App.css'
-import AddFriend from './modals/AddFriend'
 import FriendContainer from './components/containers/friendContainer'
+import Navbar from './components/containers/nav'
 
 function App() {
 
   return (
-    <div className='bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-violet-400 via-violet-800 to-violet-900 py-8 flex flex-col gap-10 justify-center items-center '>
-      <div className='flex flex-col gap-5 items-center'>
-        <h1 className='text-5xl font-semibold'>Friend Lister</h1>
-        <h2 className='text-2xl'>Add your friends and see the details</h2>
-        <AddFriend/>
-      </div>
-      <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-        <FriendContainer/>
-        <FriendContainer/>
-        <FriendContainer/>
-        <FriendContainer/>    
-      </div>
+    <div className='bg-[#000910] h-full'>
+      <Navbar/>
+      <main className='py-28 px-11 w-full text-center flex flex-col gap-10 items-center justify-center'>
+        <div className='flex flex-col gap-5 items-center'>
+          <h1 className='text-5xl font-semibold'>Friend Lister</h1>
+          <h2 className='text-2xl'>Add your friends and see the details</h2>
+        </div>
+        <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3'>
+          <FriendContainer/>
+          <FriendContainer/>
+          <FriendContainer/>
+          <FriendContainer/>
+        </div>
+      </main>
     </div>
   )
 }
