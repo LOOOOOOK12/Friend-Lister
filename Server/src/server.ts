6f41f44c83e6a57d462e9ed6bba86885d9b6ee99
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-30 18:08:53 +0800

    removed other codde

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,21 +1,16 @@
-import "dotenv/config"
+import app from "./app"
+import env from "./util/validateEnv"
 import mongoose from 'mongoose'
-import express from 'express'
-const app = express()
 
-app.get("/" ,(req,res) => {
-    res.send("Hello")
-})
+const port = env.PORT;
 
-const port = process.env.PORT;
-
-mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+mongoose.connect(env.MONGO_CONNECTION_STRING)
     .then(() => {
         console.log("Mongoose Connected")
         app.listen(port, () => {
-            console.log("server running in port " + port)
+            console.log("Server running in port " + port)
         })
     })
-
+    .catch(console.error)
 
 
