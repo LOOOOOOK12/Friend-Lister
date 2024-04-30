bee3ac726188bd0808cd58b45b02259636bae8c3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-04-30 18:09:09 +0800

    added friend models

---
+++ Server/src/models/friend.ts
@@ -0,0 +1,13 @@
+import { InferSchemaType, model, Schema } from "mongoose";
+
+const friendSchema = new Schema({
+    name: { type: String, required: true },
+    age: { type: Number },
+    gender: { type: String },
+    description: {type: String},
+
+}, { timestamps:true })
+
+type Friend = InferSchemaType<typeof friendSchema>
+
+export default model<Friend>("Friend", friendSchema)
