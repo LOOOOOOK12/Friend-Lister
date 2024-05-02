caf1dc5b2bfce6c6c8514c099f9201fe9f92500b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-02 12:03:48 +0200

    anchor tag in h1

--- Client/src/components/containers/nav.tsx
+++ Client/src/components/containers/nav.tsx
@@ -3,7 +3,7 @@ import AddFriend from '@/modals/AddFriend'
 function nav() {
     return (
         <nav className='bg-[#000910] w-full fixed top-0 z-10 flex justify-between items-center border-b-[.01rem] border-b-[#424242] px-11 py-3'>
-            <h1 className='text-3xl'>Fren</h1>
+            <a href='#Home'><h1 className='text-3xl'>Fren</h1></a>
             <AddFriend/>
         </nav>
     )
