adca459b7806666f419a084ba3c4e3183d1c3aac
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-03 17:32:07 +0800

    Ellipsis added

--- Client/src/modals/FriendProfile.tsx
+++ Client/src/modals/FriendProfile.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import { Ellipsis } from 'lucide-react';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
+import EditFriend from './EditFriend';
+import DeleteFriend from './DeleteFriend';
+import JS from '@/assets/sampleimages/js.png'
 
 function FriendProfile() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button className='bg-transparent border-[0.01rem]'>...</Button>
+                <Button className='bg-transparent border-[0.01rem]'><Ellipsis/></Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                    <DialogTitle>Add your Friend</DialogTitle>
-                    <DialogDescription>Enter your details about your friend!!</DialogDescription>
+                <DialogHeader className='flex items-center'>
+                    <img src={JS} className='h-24 w-24 rounded-md' />
+                    <DialogTitle className='text-3xl font-semibold'>Micahel Jackstone</DialogTitle>
+                    <DialogDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quas velit odio </DialogDescription>
                 </DialogHeader>
+                <DialogFooter>
+                    <EditFriend/>
+                    <DeleteFriend/>
+                </DialogFooter>
             </DialogContent>
-            <DialogFooter>
-
-            </DialogFooter>
         </Dialog>
     )
 }
