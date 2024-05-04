3220da6213e62018712c955a605bc0a4274cd4aa
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-04 17:30:03 +0800

    added textarea component

---
+++ Client/src/components/containers/textArea.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from '@radix-ui/react-label';
+
+interface Props {
+    labelName: string;
+    placeholder?: string;
+    value: string;
+    onChange?: (value: string) => void;
+}
+
+function TextArea({ labelName, placeholder, value, onChange }: Props) {
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        if (onChange) {
+            onChange(event.target.value);
+        }
+    };
+
+    return (
+        <div className="flex flex-col gap-2">
+            <Label>{labelName}</Label>
+            <Textarea
+                className='h-full w-full'
+                placeholder={placeholder}
+                onChange={handleChange}
+                value={value.toString()}
+            />
+        </div>
+    );
+}
+
+export default TextArea;
