6a8a41262a63ae2f55cb194a4ff4221b563f481a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-07 22:57:57 +0800

    description and TextArea added

--- Client/src/modals/EditFriend.tsx
+++ Client/src/modals/EditFriend.tsx
@@ -10,15 +10,25 @@ import {
 import Labels from "@/components/containers/labels"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
+import TextArea from '@/components/containers/textArea';
+import { Description } from "@radix-ui/react-dialog";
 
 function EditFriend() {
 
-    const [editFriend, seteditFriend] = useState({
+    const [editFriend, setEditFriend] = useState({
         name: '',
         age:'',
         birthday:'',
         picture:'',
+        description:"",
     })
+
+    const handleChange = (key: string, value: string) => {
+        setEditFriend(prevState => ({
+            ...prevState,
+            [key]: value,
+        }));
+    };
 
     return (
         <Dialog>
@@ -55,6 +65,12 @@ function EditFriend() {
                             labelName="Picture"
                             type="file"
                             value={editFriend.picture}
+                        />
+                        <TextArea
+                            labelName='Describe your Friend'
+                            placeholder="Etc"
+                            onChange={(value) => handleChange('description', value)}
+                            value={editFriend.description}
                         />
                         <DialogFooter>
                             <Button type="submit">Save changes</Button>
