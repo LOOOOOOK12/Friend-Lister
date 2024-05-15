00b348fd8cb974fb29c6247805ce3fb1feb2472d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-15 17:16:48 +0800

    friends controllers created

---
+++ Server/src/controllers/friends-controllers.ts
@@ -0,0 +1,31 @@
+import { RequestHandler } from "express";
+import FriendModel from "../models/friend";
+
+export const getFriends: RequestHandler = async (req,res, next) => {
+    try{
+        const friends = await FriendModel.find().exec()
+        res.status(200).json(friends);
+    } catch(error){
+        next(error)
+    }
+}
+
+export const createFriend: RequestHandler = async (req, res, next) => {
+    const name = req.body.name
+    const age = req.body.age
+    const gender = req.body.gender
+    const description = req.body.description
+    
+    try {
+        const newFriend = await FriendModel.create({
+            name: name,
+            age: age,
+            gender: gender,
+            description: description,
+        })
+
+        res.status(201).json(newFriend)
+    } catch (error) {
+        next(error)
+    }
+}
