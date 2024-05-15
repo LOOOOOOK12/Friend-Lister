27be7992ab1b83e35b3ac1a9ac9b78a92c866f67
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-05-15 17:16:35 +0800

    fixes

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,12 +1,22 @@
 import "dotenv/config"
-import express from 'express'
-import FriendModel from './models/friend'
+import express, { Request, Response, NextFunction } from 'express'
+import friendsRoutes from "./routes/friends-routes"
 
 const app = express()
 
-app.get("/" , async (req,res) => {
-    const friends = await FriendModel.find().exec()
-    res.status(200).json(friends);
+app.use(express.json())
+
+app.use("/api/friends", friendsRoutes)
+
+app.use((req, res, next) => {
+    next(Error("Endpoint not found"))
+})
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.log(error)
+    let errorMessage ="Unkown error"
+    if (error instanceof Error) errorMessage = error.message;
+    res.status(500).json({error: errorMessage})
 })
 
 export default app;
