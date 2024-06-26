cb9f4d64fcc62cca895b026ecdbe13f0f395cc7a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-26 17:00:56 +0800

    bg changes, z-index added, circles addded

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,14 +5,16 @@ import Navbar from './components/containers/nav'
 function App() {
 
   return (
-    <div className='bg-[#000910] h-full'>
+    <div className='bg-others-background h-full'>
       <Navbar/>
-      <main className='py-28 px-11 w-full text-center flex flex-col gap-10 items-center justify-center' id='Home'>
+      <main className='py-28 px-11 w-full z-10 text-center flex flex-col gap-10 items-center justify-center relative overflow-hidden' id='Home'>
+      <div className='absolute overflow-hidden -z-10 right-[2rem] top-0 size-72 bg-others-accent rounded-full blur-2xl opacity-[0.2]'></div>
         <div className='flex flex-col gap-5 items-center' >
           <h1 className='text-5xl font-semibold'>Friend Lister</h1>
-          <h2 className='text-2xl'>Add your friends and see the details</h2>
+          <h2 className='text-2xl text-others-accent'>Add your friends and see the details</h2>
         </div>
         <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3'>
+          <FriendContainer/>
           <FriendContainer/>
           <FriendContainer/>
           <FriendContainer/>
