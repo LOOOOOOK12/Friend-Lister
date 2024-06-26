a25fa440d1194dcb1c1b26e41d70aef16e77467a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-26 17:01:37 +0800

    bg changes, z-index

--- Client/src/components/containers/nav.tsx
+++ Client/src/components/containers/nav.tsx
@@ -2,7 +2,7 @@ import AddFriend from '@/modals/AddFriend'
 
 function nav() {
     return (
-        <nav className='bg-[#000910] w-full fixed top-0 z-10 flex justify-between items-center border-b-[.01rem] border-b-[#424242] px-11 py-3'>
+        <nav className='bg-others-background w-full fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-11 py-3'>
             <a href='#Home'><h1 className='text-3xl font-semibold'>Fren</h1></a>
             <AddFriend/>
         </nav>
