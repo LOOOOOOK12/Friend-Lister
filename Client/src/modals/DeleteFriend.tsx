d26aa31b2532013502db32d3a06eacf0ff9505dc
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-26 17:02:50 +0800

    changes in color of buttons

--- Client/src/modals/DeleteFriend.tsx
+++ Client/src/modals/DeleteFriend.tsx
@@ -13,15 +13,15 @@ function DeleteFriend() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button className="">Delete</Button>
+                <Button className="bg-others-accent border-none hover:bg-[#995177]">Delete</Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px] flex flex-col items-center">
                 <DialogHeader>
                     <DialogTitle>Are you sure to delete your friend?</DialogTitle>
                 </DialogHeader>
                 <DialogFooter className="flex items-center justify-center">
-                    <Button type="submit">Yes</Button>
-                    <Button>No</Button>
+                    <Button type="submit" className="bg-green-500 border-none hover:bg-green-600">Yes</Button>
+                    <Button variant="destructive">No</Button>
                 </DialogFooter>
             </DialogContent>
         </Dialog>
