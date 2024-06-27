f80dbcd98bf32839443842aaafbca3a9a4aae7fa
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-27 17:19:33 +0800

    added httpsError

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,6 +2,8 @@ import "dotenv/config"
 import express, { Request, Response, NextFunction } from 'express'
 import friendsRoutes from "./routes/friends-routes"
 import morgan from "morgan"
+import createHttpError, { isHttpError } from "http-errors"
+
 
 const app = express()
 
@@ -12,14 +14,18 @@ app.use(express.json())
 app.use("/api/friends", friendsRoutes)
 
 app.use((req, res, next) => {
-    next(Error("Endpoint not found"))
+    next(createHttpError(404,"Endpoint not found"))
 })
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.log(error)
     let errorMessage ="Unkown error"
-    if (error instanceof Error) errorMessage = error.message;
-    res.status(500).json({error: errorMessage})
+    let statusCode = 500
+    if(isHttpError(error)){
+        statusCode = error.status
+        errorMessage = error.message
+    }
+    res.status(statusCode).json({ error: errorMessage})
 })
 
 export default app;
