cf03614d8b7068bb92ebd776af0d9e04730bf027
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-28 17:32:26 +0800

    added update and delete controllerrs

--- Server/src/controllers/friends-controllers.ts
+++ Server/src/controllers/friends-controllers.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import FriendModel from "../models/friend";
 import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 //Load Friends
 export const getFriends: RequestHandler = async (req,res, next) => {
@@ -17,7 +18,15 @@ export const getFriend: RequestHandler = async (req, res, next) => {
     const friendId = req.params.friendId
 
     try {
+        if(!mongoose.isValidObjectId(friendId)){
+            throw createHttpError(400, "Invalid Friend ID")
+        }
+
         const friend = await FriendModel.findById(friendId).exec()
+
+        if(!friend){
+            throw createHttpError(404,"Friend Not Found")
+        }
         res.status(200).json(friend)
     } catch (error) {
         next(error)
@@ -59,13 +68,74 @@ export const createFriend: RequestHandler< unknown, unknown, CreateFriendBody, u
     }
 }
 
+interface UpdateFriendParams {
+    friendId: string,
+}
+
+interface UpdateFriendBody {
+    name?: string,
+    age?: number,
+    gender?: string,
+    description?: string,
+    picture?: string,
+}
+
+export const updateFriend: RequestHandler <UpdateFriendParams, unknown, UpdateFriendBody, unknown> = async (req, res, next ) => {
+    const friendId = req.params.friendId
+    const newName = req.body.name
+    const newAge = req.body.age
+    const newGender = req.body.gender
+    const newDescription = req.body.description
+    const newPicture = req.body.picture
+    
+    try {
+        if(!mongoose.isValidObjectId(friendId)){
+            throw createHttpError(400, "Invalid Friend ID")
+        }
+
+        if(!newName){
+            throw createHttpError(400,"Friend must have a Name!!")
+        }
+
+        const friend = await FriendModel.findById(friendId).exec();
+
+        if(!friend){
+            throw createHttpError(404, "Friend not found")
+        }
+
+        friend.name = newName
+        friend.age = newAge
+        friend.gender = newGender
+        friend.description = newDescription
+        friend.picture = newPicture
+
+        const updatedFriend = await friend.save()
+
+        res.status(200).json(updatedFriend)
+
+    } catch (error) {
+        next(error)
+    }
+}
+
 //delete Friend function
-export const deleteFriend: RequestHandler = async (req, res, next )=> {
+export const deleteFriend: RequestHandler = async ( req, res, next )=> {
     const friendId = req.params.friendId
 
     try {
-        const friend = await FriendModel.findByIdAndDelete(friendId).exec()
-        res.status(200).json(friend)
+
+        if(!mongoose.isValidObjectId(friendId)){
+            throw createHttpError(400, "Invalid Friend ID")
+        }
+
+        const friend = await FriendModel.findById(friendId).exec()
+
+        if(!friend){
+            throw createHttpError(404, "Friend not found")
+        }
+        
+        res.sendStatus(204)
+
     } catch (error) {
         next(error)
     }
