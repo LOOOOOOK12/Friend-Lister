7b5ab9c8343a27b032e9a0ac887846ae9e0ae184
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-06-29 18:03:29 +0800

    added friends interface

---
+++ Client/src/models/friends.ts
@@ -0,0 +1,10 @@
+export interface Friends {
+    _id: string,
+    name: string,
+    age: string,
+    gender: string,
+    description: string,
+    picture: string,
+    createdAt: string,
+    updatedAt: string,
+}
