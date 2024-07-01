197511f0bd547a2f0b6393ed8baf1120c0e2f54a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-01 17:07:43 +0800

    changes in a tag text color

--- Client/src/components/containers/nav.tsx
+++ Client/src/components/containers/nav.tsx
@@ -3,7 +3,7 @@ import AddFriend from '@/modals/AddFriend'
 function nav() {
     return (
         <nav className='bg-others-background w-full bg-opacity-90 backdrop-blur fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-11 py-3'>
-            <a href='#Home'><h1 className='text-3xl font-semibold'>Fren</h1></a>
+            <a href='#Home'><h1 className='text-4xl font-bold text-transparent bg-clip-text inline-block bg-gradient-to-r from-others-primary via-others-secondary to-others-accent'>Fren</h1></a>
             <AddFriend/>
         </nav>
     )
