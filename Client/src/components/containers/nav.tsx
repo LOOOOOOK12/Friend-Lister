bc5a803060e0e25cb5bde1eba3ad897ce000c32f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-06 21:41:41 +0800

    updates

--- Client/src/components/containers/nav.tsx
+++ Client/src/components/containers/nav.tsx
@@ -4,7 +4,7 @@ function nav() {
     return (
         <nav className='bg-others-background w-full bg-opacity-90 backdrop-blur fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-11 py-3'>
             <a href='#Home'><h1 className='text-4xl font-bold text-transparent bg-clip-text inline-block bg-gradient-to-r from-others-primary via-others-secondary to-others-accent'>Fren</h1></a>
-            <AddFriend/>
+            <AddFriend />
         </nav>
     )
 }
