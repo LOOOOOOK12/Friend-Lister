edf5a9b8cc039bd4175c65e82fcb685052f18199
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-06 21:44:06 +0800

    added check friennds route

--- Server/src/routes/friends-routes.ts
+++ Server/src/routes/friends-routes.ts
@@ -7,6 +7,8 @@ router.get("/" , FriendsController.getFriends)
 
 router.get("/:friendId", FriendsController.getFriend)
 
+// router.get("/check-friend", FriendsController.checkFriends);
+
 router.post("/", FriendsController.createFriend)
 
 router.patch("/:friendId",FriendsController.updateFriend)
