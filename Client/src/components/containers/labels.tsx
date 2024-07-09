c014478c2404085d2c67b7b3cf1f94e3ef1fcb38
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-09 16:49:47 +0800

    fixes in modal

--- Client/src/components/containers/labels.tsx
+++ Client/src/components/containers/labels.tsx
@@ -13,19 +13,22 @@ interface Props {
 function Labels({ labelName, type, placeholder, value, onChange }: Props) {
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (onChange) {
-        onChange(event.target.value);
+            onChange(event.target.value);
         }
     };
 
+    // Ensure value is a string before passing it to Input component
+    const displayValue = value !== null && value !== undefined ? value.toString() : '';
+
     return (
-        <div className="flex flex-col gap-2 bg-[]">
+        <div className="flex flex-col gap-2">
             <Label>{labelName}</Label>
             <Input
                 id={labelName.toLowerCase()}
                 placeholder={placeholder}
                 type={type}
                 onChange={handleChange}
-                value={value.toString()} 
+                value={displayValue}
                 className="col-span-3"
             />
         </div>
