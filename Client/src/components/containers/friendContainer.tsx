c4db237c400986e2e47f35600b6c8c472c56a44c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-09 16:49:40 +0800

    added on delete and friend modal

--- Client/src/components/containers/friendContainer.tsx
+++ Client/src/components/containers/friendContainer.tsx
@@ -7,22 +7,24 @@ import {
 import FriendProfile from "@/modals/FriendProfile"
 import { Friends } from '../../models/friends';
 
-interface FriendsProps {
-    friends: Friends,
+interface FriendContainerProps {
+    friends: Friends;
+    onDeleteFriend: (friendId: string) => void;
 }
 
-function friendContainer({ friends }: FriendsProps) {
+const FriendContainer: React.FC<FriendContainerProps> = ({ friends, onDeleteFriend }) => {
+    
     return (
         <Card className="bg-others-container border border-[#303051] bg-opacity-75 backdrop-blur">
             <CardHeader className="items-end">
-                <FriendProfile friends={friends}/>
+                <FriendProfile friends={friends} onDeleteFriend={onDeleteFriend} />
             </CardHeader>
             <CardContent className="flex flex-col justify-center items-center gap-4">
-                <img src="src\assets\sampleimages\js.png" alt="test" className=" w-full rounded-md" />
+                <img src="src/assets/sampleimages/js.png" alt="test" className="w-full rounded-md" />
                 <CardTitle>{friends.name}</CardTitle>
             </CardContent>
         </Card>
-    )
+    );
 }
 
-export default friendContainer
+export default FriendContainer;
