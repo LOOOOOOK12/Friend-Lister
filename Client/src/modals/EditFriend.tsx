50821e1be4bf3e02356ed721e2ba4107afb1d54f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-09 16:50:51 +0800

    added props and previous data

--- Client/src/modals/EditFriend.tsx
+++ Client/src/modals/EditFriend.tsx
@@ -8,23 +8,38 @@ import {
 } from "@/components/ui/dialog"
 import Labels from "@/components/containers/labels"
 import { Button } from "@/components/ui/button"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import TextArea from '@/components/containers/textArea';
 
-function EditFriend() {
+interface EditFriendProps {
+    friendData: {
+        name: string;
+        age: string;
+        gender: string;
+        birthday: string;
+        picture: string;
+        description: string;
+    };
+}
+
+function EditFriend({ friendData }: EditFriendProps) {
 
     const [editFriend, setEditFriend] = useState({
         name: '',
-        age:'',
-        gender:'',
-        birthday:'',
-        picture:'',
-        description:"",
-    })
+        age: '',
+        gender: '',
+        birthday: '',
+        picture: '',
+        description: "",
+    });
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+    useEffect(() => {
+        setEditFriend(friendData);
+    }, [friendData]);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(editFriend); 
+        console.log(editFriend);
     }
 
     const handleChange = (key: string, value: string) => {
@@ -43,54 +58,54 @@ function EditFriend() {
                 <DialogHeader>
                     <DialogTitle>Edit your friend's details</DialogTitle>
                 </DialogHeader>
-                    <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-                        <Labels
-                            labelName="Name"
-                            type="text"
-                            placeholder="Juan Dela Cruz"
-                            value={editFriend.name}
-                            onChange={(value) => handleChange('name', value)}
-                        />
-                        <Labels
-                            labelName="Age"
-                            type="text"
-                            placeholder="12"
-                            value={editFriend.age}
-                            onChange={(value) => handleChange('age', value)}
-                        />
-                        <Labels
-                                labelName="Gender"
-                                type="text"
-                                placeholder="12"
-                                value={editFriend.gender}
-                                onChange={(value) => handleChange('gender', value)}
-                            />
-                        <Labels
-                            labelName="Birthday"
-                            type="date"
-                            placeholder="12"
-                            value={editFriend.birthday}
-                            onChange={(value) => handleChange('birthday', value)}
-                        />
-                        <TextArea
-                            labelName='Describe your Friend'
-                            placeholder="My friend is..."
-                            value={editFriend.description}
-                            onChange={(value) => handleChange('description', value)}
-                        />
-                        <Labels
-                            labelName="Picture"
-                            type="file"
-                            value={editFriend.picture}
-                            onChange={(value) => handleChange('picture', value)}
-                        />
-                        <DialogFooter>
-                            <Button type="submit" className="bg-others-secondary border-none hover:bg-[#5a255a]">Save changes</Button>
-                        </DialogFooter>
-                    </form>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+                    <Labels
+                        labelName="Name"
+                        type="text"
+                        placeholder="Juan Dela Cruz"
+                        value={editFriend.name || ''}
+                        onChange={(value) => handleChange('name', value)}
+                    />
+                    <Labels
+                        labelName="Age"
+                        type="text"
+                        placeholder="12"
+                        value={editFriend.age || ''}
+                        onChange={(value) => handleChange('age', value)}
+                    />
+                    <Labels
+                        labelName="Gender"
+                        type="text"
+                        placeholder="Gender"
+                        value={editFriend.gender || ''}
+                        onChange={(value) => handleChange('gender', value)}
+                    />
+                    <Labels
+                        labelName="Birthday"
+                        type="date"
+                        placeholder="Birthday"
+                        value={editFriend.birthday || ''}
+                        onChange={(value) => handleChange('birthday', value)}
+                    />
+                    <TextArea
+                        labelName='Describe your Friend'
+                        placeholder="My friend is..."
+                        value={editFriend.description || ''}
+                        onChange={(value) => handleChange('description', value)}
+                    />
+                    <Labels
+                        labelName="Picture"
+                        type="file"
+                        value={editFriend.picture || ''}
+                        onChange={(value) => handleChange('picture', value)}
+                    />
+                    <DialogFooter>
+                        <Button type="submit" className="bg-others-secondary border-none hover:bg-[#5a255a]">Save changes</Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
-    )
+    );
 }
 
-export default EditFriend
+export default EditFriend;
