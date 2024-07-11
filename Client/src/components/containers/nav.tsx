ffb4380902a9b2ded6cf17a4fda10825132ce336
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-11 15:18:53 +0800

    added NavBarProps

--- Client/src/components/containers/nav.tsx
+++ Client/src/components/containers/nav.tsx
@@ -1,12 +1,17 @@
-import AddFriend from '@/modals/AddFriend'
+import AddFriend from '@/modals/AddFriend';
+import { Friends } from '@/models/friends';
 
-function nav() {
+interface NavbarProps {
+    onAddFriend: (newFriend: Friends) => void;
+}
+
+function Navbar({ onAddFriend }: NavbarProps) {
     return (
         <nav className='bg-others-background w-full bg-opacity-90 backdrop-blur fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-11 py-3'>
             <a href='#Home'><h1 className='text-4xl font-bold text-transparent bg-clip-text inline-block bg-gradient-to-r from-others-primary via-others-secondary to-others-accent'>Fren</h1></a>
-            <AddFriend />
+            <AddFriend onAddFriend={onAddFriend} />
         </nav>
-    )
+    );
 }
 
-export default nav
+export default Navbar;
