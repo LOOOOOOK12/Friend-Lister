175e670eea4b3c2f5ca4fec0c8cab703aa9d3bde
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-15 16:20:59 +0800

    changes in server

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,16 +1,14 @@
-import app from "./app"
-import env from "./util/validateEnv"
-import mongoose from 'mongoose'
+import app from "./app";
+import env from "./util/validateEnv";
+import mongoose from 'mongoose';
 
 const port = env.PORT;
 
 mongoose.connect(env.MONGO_CONNECTION_STRING)
     .then(() => {
-        console.log("Mongoose Connected")
+        console.log("Mongoose Connected");
         app.listen(port, () => {
-            console.log("Server running in port " + port)
-        })
+            console.log("Server running in port " + port);
+        });
     })
-    .catch(console.error)
-
-
+    .catch(console.error);
