ffcda4339a482937d95059f2ac9b878f0228f03f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-18 16:10:24 +0800

    added a search input

--- Client/src/components/containers/nav.tsx
+++ Client/src/components/containers/nav.tsx
@@ -1,15 +1,30 @@
+// Navbar.tsx
 import AddFriend from '@/modals/AddFriend';
 import { Friends } from '@/models/friends';
+import { Input } from '@/components/ui/input';
 
 interface NavbarProps {
     onAddFriend: (newFriend: Friends) => void;
+    onFindFriendChange: (searchTerm: string) => void;
 }
 
-function Navbar({ onAddFriend }: NavbarProps) {
+function Navbar({ onAddFriend, onFindFriendChange }: NavbarProps) {
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        onFindFriendChange(event.target.value);
+    };
+
     return (
-        <nav className='bg-others-background w-full bg-opacity-90 backdrop-blur fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-11 py-3'>
+        <nav className='bg-others-background w-full bg-opacity-90 backdrop-blur fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-5 py-3'>
             <a href='#Home'><h1 className='text-4xl font-bold text-transparent bg-clip-text inline-block bg-gradient-to-r from-others-primary via-others-secondary to-others-accent'>Fren</h1></a>
-            <AddFriend onAddFriend={onAddFriend} />
+            <div className=' flex gap-3'>
+                <Input
+                    placeholder='Search'
+                    type='search'
+                    onChange={handleSearchChange}
+                />
+                <AddFriend onAddFriend={onAddFriend} />
+            </div>
         </nav>
     );
 }
