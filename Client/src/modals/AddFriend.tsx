2159a5dfadd9499fe5104cac24ac6e2100d53129
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-20 16:07:41 +0800

    applied react hook form (testing)

--- Client/src/modals/AddFriend.tsx
+++ Client/src/modals/AddFriend.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import Labels from '@/components/containers/labels';
@@ -11,44 +12,31 @@ interface AddFriendProps {
 }
 
 function AddFriend({ onAddFriend }: AddFriendProps) {
-    const [friend, setFriend] = useState({
-        name: '',
-        age: '',
-        gender: '',
-        birthday: '',
-        picture: '',
-        description: '',
-    });
+    const { control, handleSubmit, reset } = useForm<Friends>();
+
     const [isOpen, setIsOpen] = useState(false);
 
-    const handleChange = (key: string, value: string) => {
-        setFriend(prevState => ({
-            ...prevState,
-            [key]: value,
-        }));
-    };
-
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>, onChange: (value: string) => void) => {
         const file = event.target.files?.[0];
         if (file) {
             const reader = new FileReader();
             reader.onloadend = () => {
                 const base64String = reader.result?.toString();
                 if (base64String) {
-                    handleChange('picture', base64String);
+                    onChange(base64String);
                 }
             };
             reader.readAsDataURL(file);
         }
     };
-    
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+
+    const onSubmit = async (data: Friends) => {
         try {
-            const newFriend = await createFriend(friend);
+            const newFriend = await createFriend(data);
             console.log("Friend added successfully:", newFriend);
             onAddFriend(newFriend);
             setIsOpen(false);
+            reset();
         } catch (error) {
             console.error("Error adding friend:", error);
         }
@@ -63,46 +51,83 @@ function AddFriend({ onAddFriend }: AddFriendProps) {
                 <DialogHeader>
                     <DialogTitle>Add your Friend</DialogTitle>
                 </DialogHeader>
-                <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
                     <div className='flex flex-col gap-2'>
-                        <Labels
-                            labelName="Name"
-                            type="text"
-                            placeholder="Juan Dela Cruz"
-                            value={friend.name}
-                            onChange={(value) => handleChange('name', value)}
+                        <Controller
+                            name="name"
+                            control={control}
+                            rules={{required: true}}
+                            render={({ field }) => (
+                                <Labels
+                                    labelName="Name"
+                                    type="text"
+                                    placeholder="Juan Dela Cruz"
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                />
+                            )}
                         />
-                        <Labels
-                            labelName="Age"
-                            type="text"
-                            placeholder="12"
-                            value={friend.age}
-                            onChange={(value) => handleChange('age', value)}
+                        <Controller
+                            name="age"
+                            control={control}
+                            render={({ field }) => (
+                                <Labels
+                                    labelName="Age"
+                                    type="text"
+                                    placeholder="12"
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                />
+                            )}
                         />
-                        <Labels
-                            labelName="Gender"
-                            type="text"
-                            placeholder="Male/Female"
-                            value={friend.gender}
-                            onChange={(value) => handleChange('gender', value)}
+                        <Controller
+                            name="gender"
+                            control={control}
+                            render={({ field }) => (
+                                <Labels
+                                    labelName="Gender"
+                                    type="text"
+                                    placeholder="Male/Female"
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                />
+                            )}
                         />
-                        <Labels
-                            labelName="Birthday"
-                            type="date"
-                            placeholder="12"
-                            value={friend.birthday}
-                            onChange={(value) => handleChange('birthday', value)}
+                        <Controller
+                            name="birthday"
+                            control={control}
+                            render={({ field }) => (
+                                <Labels
+                                    labelName="Birthday"
+                                    type="date"
+                                    placeholder="12"
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                />
+                            )}
                         />
-                        <TextArea
-                            labelName='Describe your Friend'
-                            placeholder="My friend is..."
-                            onChange={(value) => handleChange('description', value)}
-                            value={friend.description}
+                        <Controller
+                            name="description"
+                            control={control}
+                            render={({ field }) => (
+                                <TextArea
+                                    labelName='Describe your Friend'
+                                    placeholder="My friend is..."
+                                    value={field.value}
+                                    onChange={field.onChange}
+                                />
+                            )}
                         />
-                        <Labels
-                            labelName="Picture"
-                            type="file"
-                            onFileChange={handleFileChange}
+                        <Controller
+                            name="picture"
+                            control={control}
+                            render={({ field }) => (
+                                <Labels
+                                    labelName="Picture"
+                                    type="file"
+                                    onFileChange={(e) => handleFileChange(e, field.onChange)}
+                                />
+                            )}
                         />
                     </div>
                     <DialogFooter>
