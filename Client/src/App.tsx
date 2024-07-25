b06f645ddd8f777232daff55a86a4c226de2ac4c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-25 17:39:58 +0800

    adjustments

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -69,7 +69,7 @@ function App() {
                 </div>
                 {searching ? (
                     <h1 className="text-2xl text-others-text">Searching...</h1>
-                ) : (
+                    ) : (
                     friends.length > 0 ? (
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 w-full h-full">
                             {friends.map((friend) => (
