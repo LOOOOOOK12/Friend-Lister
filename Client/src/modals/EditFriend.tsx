5cc7f6e1ec38d39f7ae6c28476d1abe779ab1fff
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-26 17:17:03 +0800

    added on close for modal

--- Client/src/modals/EditFriend.tsx
+++ Client/src/modals/EditFriend.tsx
@@ -8,10 +8,11 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import Labels from "@/components/containers/labels";
 import { Button } from "@/components/ui/button";
+import Labels from '@/components/containers/labels';
 import TextArea from '@/components/containers/textArea';
-import { updateFriend } from '@/network/friends_api'; 
+import { Friends } from '../models/friends';
+import * as FriendsApi from "../network/friends_api";
 
 interface EditFriendProps {
     friendId: string;
@@ -23,13 +24,15 @@ interface EditFriendProps {
         picture: string;
         description: string;
     };
+    onUpdateFriend: (updatedFriend: Friends) => void;
 }
 
-function EditFriend({ friendId, initialFriendData }: EditFriendProps) {
-    const { control, handleSubmit, setValue, formState: { errors } } = useForm({
-        defaultValues: initialFriendData
+const EditFriend: React.FC<EditFriendProps> = ({ friendId, initialFriendData, onUpdateFriend }) => {
+    const { control, handleSubmit, reset, formState: { errors } } = useForm({
+        defaultValues: initialFriendData,
     });
 
+    const [loading, setLoading] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>, onChange: (value: string) => void) => {
@@ -46,138 +49,143 @@ function EditFriend({ friendId, initialFriendData }: EditFriendProps) {
         }
     };
 
-    const onSubmit = async (data: typeof initialFriendData) => {
+    const onSubmit = async (data: any) => {
+        setLoading(true);
         try {
-            await updateFriend(friendId, data);
-            console.log("Friend updated successfully:", data);
-            window.location.reload();
+            const updatedFriend = await FriendsApi.updateFriend(friendId, data);
+            onUpdateFriend(updatedFriend);
+            setIsOpen(false);
         } catch (error) {
             console.error("Error updating friend:", error);
+        } finally {
+            setLoading(false);
         }
     };
 
     useEffect(() => {
-        for (const key in initialFriendData) {
-            setValue(key as keyof typeof initialFriendData, initialFriendData[key as keyof typeof initialFriendData]);
-        }
-    }, [initialFriendData, setValue]);
+        reset(initialFriendData);
+    }, [initialFriendData, reset]);
 
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogTrigger asChild>
-                <Button className="bg-others-secondary border-none hover:bg-[#511f51]" onClick={() => setIsOpen(true)}>Edit</Button>
+                <Button className="bg-others-secondary border-none hover:bg-[#622962]" onClick={() => setIsOpen(true)}>Edit</Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
+            <DialogContent className="sm:max-w-[500px]">
                 <DialogHeader>
-                    <DialogTitle>Edit your friend's details</DialogTitle>
+                    <DialogTitle>Edit Friend</DialogTitle>
                 </DialogHeader>
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
-                    <Controller
-                        name="name"
-                        control={control}
-                        rules={{ required: 'Name is required' }}
-                        render={({ field }) => (
-                            <>
+                    <div className='flex flex-col gap-2'>
+                        <Controller
+                            name="name"
+                            control={control}
+                            rules={{ required: 'Name is required' }}
+                            render={({ field }) => (
+                                <>
+                                    <Labels
+                                        labelName="Name"
+                                        type="text"
+                                        placeholder="Juan Dela Cruz"
+                                        value={field.value}
+                                        onChange={field.onChange}
+                                        className={errors.name ? 'border-red-500' : ''}
+                                    />
+                                    {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+                                </>
+                            )}
+                        />
+                        <Controller
+                            name="age"
+                            control={control}
+                            rules={{ required: 'Age is required', pattern: { value: /^[0-9]+$/, message: 'Age must be a number' } }}
+                            render={({ field }) => (
+                                <>
+                                    <Labels
+                                        labelName="Age"
+                                        type="text"
+                                        placeholder="12"
+                                        value={field.value}
+                                        onChange={field.onChange}
+                                        className={errors.age ? 'border-red-500' : ''}
+                                    />
+                                    {errors.age && <p className="text-red-500">{errors.age.message}</p>}
+                                </>
+                            )}
+                        />
+                        <Controller
+                            name="gender"
+                            control={control}
+                            rules={{ required: 'Gender is required' }}
+                            render={({ field }) => (
+                                <>
+                                    <Labels
+                                        labelName="Gender"
+                                        type="text"
+                                        placeholder="Male/Female"
+                                        value={field.value}
+                                        onChange={field.onChange}
+                                        className={errors.gender ? 'border-red-500' : ''}
+                                    />
+                                    {errors.gender && <p className="text-red-500">{errors.gender.message}</p>}
+                                </>
+                            )}
+                        />
+                        <Controller
+                            name="birthday"
+                            control={control}
+                            rules={{ required: 'Birthday is required' }}
+                            render={({ field }) => (
+                                <>
+                                    <Labels
+                                        labelName="Birthday"
+                                        type="date"
+                                        value={field.value}
+                                        onChange={field.onChange}
+                                        className={errors.birthday ? 'border-red-500' : ''}
+                                    />
+                                    {errors.birthday && <p className="text-red-500">{errors.birthday.message}</p>}
+                                </>
+                            )}
+                        />
+                        <Controller
+                            name="description"
+                            control={control}
+                            rules={{ required: 'Description is required' }}
+                            render={({ field }) => (
+                                <>
+                                    <TextArea
+                                        labelName='Describe your Friend'
+                                        placeholder="My friend is..."
+                                        value={field.value}
+                                        onChange={field.onChange}
+                                        className={errors.description ? 'border-red-500' : ''}
+                                    />
+                                    {errors.description && <p className="text-red-500">{errors.description.message}</p>}
+                                </>
+                            )}
+                        />
+                        <Controller
+                            name="picture"
+                            control={control}
+                            render={({ field }) => (
                                 <Labels
-                                    labelName="Name"
-                                    type="text"
-                                    placeholder="Juan Dela Cruz"
-                                    value={field.value}
-                                    onChange={field.onChange}
-                                    className={errors.name ? 'border-red-500' : ''}
+                                    labelName="Picture"
+                                    type="file"
+                                    onFileChange={(e) => handleFileChange(e, field.onChange)}
                                 />
-                                {errors.name && <p className="text-red-500">{errors.name.message}</p>}
-                            </>
-                        )}
-                    />
-                    <Controller
-                        name="age"
-                        control={control}
-                        rules={{ required: 'Age is required', pattern: { value: /^[0-9]+$/, message: 'Age must be a number' } }}
-                        render={({ field }) => (
-                            <>
-                                <Labels
-                                    labelName="Age"
-                                    type="text"
-                                    placeholder="12"
-                                    value={field.value}
-                                    onChange={field.onChange}
-                                    className={errors.age ? 'border-red-500' : ''}
-                                />
-                                {errors.age && <p className="text-red-500">{errors.age.message}</p>}
-                            </>
-                        )}
-                    />
-                    <Controller
-                        name="gender"
-                        control={control}
-                        rules={{ required: 'Gender is required' }}
-                        render={({ field }) => (
-                            <>
-                                <Labels
-                                    labelName="Gender"
-                                    type="text"
-                                    placeholder="Male/Female/Other"
-                                    value={field.value}
-                                    onChange={field.onChange}
-                                    className={errors.gender ? 'border-red-500' : ''}
-                                />
-                                {errors.gender && <p className="text-red-500">{errors.gender.message}</p>}
-                            </>
-                        )}
-                    />
-                    <Controller
-                        name="birthday"
-                        control={control}
-                        rules={{ required: 'Birthday is required' }}
-                        render={({ field }) => (
-                            <>
-                                <Labels
-                                    labelName="Birthday"
-                                    type="date"
-                                    value={field.value}
-                                    onChange={field.onChange}
-                                    className={errors.birthday ? 'border-red-500' : ''}
-                                />
-                                {errors.birthday && <p className="text-red-500">{errors.birthday.message}</p>}
-                            </>
-                        )}
-                    />
-                    <Controller
-                        name="description"
-                        control={control}
-                        rules={{ required: 'Description is required' }}
-                        render={({ field }) => (
-                            <>
-                                <TextArea
-                                    labelName='Describe your Friend'
-                                    placeholder="My friend is..."
-                                    value={field.value}
-                                    onChange={field.onChange}
-                                    className={errors.description ? 'border-red-500' : ''}
-                                />
-                                {errors.description && <p className="text-red-500">{errors.description.message}</p>}
-                            </>
-                        )}
-                    />
-                    <Controller
-                        name="picture"
-                        control={control}
-                        render={({ field }) => (
-                            <Labels
-                                labelName="Picture"
-                                type="file"
-                                onFileChange={(e) => handleFileChange(e, field.onChange)}
-                            />
-                        )}
-                    />
+                            )}
+                        />
+                    </div>
                     <DialogFooter>
-                        <Button type="submit" className="bg-others-secondary border-none hover:bg-[#5a255a]">Save changes</Button>
+                        <Button type="submit" className='w-full bg-others-primary border-none text-slate-950 hover:bg-[#7581c5]' loading={loading}>
+                            Save Changes
+                        </Button>
                     </DialogFooter>
                 </form>
             </DialogContent>
         </Dialog>
     );
-}
+};
 
 export default EditFriend;
