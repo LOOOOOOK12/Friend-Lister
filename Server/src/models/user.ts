790b1e368273a82e8654bcd218976936c3335c34
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-30 16:39:02 +0800

    added users schema

---
+++ Server/src/models/user.ts
@@ -0,0 +1,11 @@
+import { model, Schema, InferSchemaType } from "mongoose";
+
+const userSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, select: false},
+    password: {type: String, required: true, unique: true, select: false},
+});
+
+type User = InferSchemaType<typeof userSchema>;
+
+export default model<User>("User",userSchema);
