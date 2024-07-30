c7578c0e8261f72f4500d92dfbb55ea053de48c4
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-30 16:39:12 +0800

    added users routes

---
+++ Server/src/routes/user-routes.ts
@@ -0,0 +1,14 @@
+import  express  from "express";
+import * as UserController from "../controllers/users-controllers"
+
+const router = express.Router();
+
+router.get("/", UserController.getAuthenticatedUser)
+
+router.post("/signup", UserController.signUp);
+
+router.post("/login", UserController.login);
+
+router.post("/logout", UserController.logout);
+
+export default router;
