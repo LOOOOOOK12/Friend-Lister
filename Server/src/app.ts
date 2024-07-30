3e2a236ca7eac3cee00531deb15b46b9843f57f9
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-07-30 16:38:25 +0800

    added sessions, authentications

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,14 +1,32 @@
 import 'dotenv/config';
 import express, { Request, Response, NextFunction } from 'express';
 import friendsRoutes from './routes/friends-routes';
+import userRoutes from "./routes/user-routes";
 import morgan from 'morgan';
 import createHttpError, { isHttpError } from 'http-errors';
+import session from 'express-session';
+import env from "./util/validateEnv";
+import MongoStore from 'connect-mongo';
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use(session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+        mongoUrl: env.MONGO_CONNECTION_STRING,
+    }),
+}));
+
+app.use('/api/users', userRoutes);
 app.use('/api/friends', friendsRoutes);
 
 app.use((req, res, next) => {
