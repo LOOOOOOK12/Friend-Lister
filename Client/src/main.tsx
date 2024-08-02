b9dce8a8e972f20a23473f9514d179dac38e4e2e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-08-02 16:20:14 +0800

    ✅ added Routes

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Login from './pages/Login.tsx'
+import Routes from './Routes/routes.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Routes/>
   </React.StrictMode>,
 )
