bece61857f6a7a8de773ae08189d9c5136e58795
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-08-05 15:52:20 +0800

    ✅ added logout temporary features

--- Client/src/components/containers/nav.tsx
+++ Client/src/components/containers/nav.tsx
@@ -1,4 +1,4 @@
-// Navbar.tsx
+import Logout from '@/modals/Logout';
 import AddFriend from '@/modals/AddFriend';
 import { Friends } from '@/models/friends';
 import { Input } from '@/components/ui/input';
@@ -17,13 +17,15 @@ function Navbar({ onAddFriend, onFindFriendChange }: NavbarProps) {
     return (
         <nav className='bg-others-background w-full bg-opacity-90 backdrop-blur fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-5 py-3'>
             <a href='#Home'><h1 className='text-4xl font-bold text-transparent bg-clip-text inline-block bg-gradient-to-r from-others-primary via-others-secondary to-others-accent'>Fren</h1></a>
-            <div className=' flex gap-3'>
                 <Input
-                    placeholder='Search'
+                    placeholder='Search friend'
                     type='search'
                     onChange={handleSearchChange}
+                    className='w-1/3'
                 />
+            <div className='flex gap-3'>
                 <AddFriend onAddFriend={onAddFriend} />
+                <Logout/>
             </div>
         </nav>
     );
