af86105928118177aac94b7af52eb66e8967061c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-08-05 15:51:13 +0800

    🚩 removed other imports

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import Login from './pages/Login.tsx'
+
 import Routes from './Routes/routes.tsx'
 import './index.css'
 
