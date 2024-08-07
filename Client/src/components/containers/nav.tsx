ddaf429e12c7252b7a926d1aa00a2f9964ea4598
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-08-07 15:49:19 +0800

    💄 updated anchor tag with image and gap for the buttons

--- Client/src/components/containers/nav.tsx
+++ Client/src/components/containers/nav.tsx
@@ -2,6 +2,7 @@ import Logout from '@/modals/Logout';
 import AddFriend from '@/modals/AddFriend';
 import { Friends } from '@/models/friends';
 import { Input } from '@/components/ui/input';
+import Friends2 from '@/assets/Friends2.png'
 
 interface NavbarProps {
     onAddFriend: (newFriend: Friends) => void;
@@ -15,15 +16,15 @@ function Navbar({ onAddFriend, onFindFriendChange }: NavbarProps) {
     };
 
     return (
-        <nav className='bg-others-background w-full bg-opacity-90 backdrop-blur fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-5 py-3'>
-            <a href='#Home'><h1 className='text-4xl font-bold text-transparent bg-clip-text inline-block bg-gradient-to-r from-others-primary via-others-secondary to-others-accent'>Fren</h1></a>
+        <nav className='bg-others-background w-full bg-opacity-90 backdrop-blur fixed top-0 z-20 flex justify-between items-center border-b-[.01rem] border-b-[#303051] px-5 py-2'>
+            <a href='#Home'><img src={Friends2} alt="Friend Lister" className='h-full' /></a>
                 <Input
                     placeholder='Search friend'
                     type='search'
                     onChange={handleSearchChange}
                     className='w-1/3'
                 />
-            <div className='flex gap-3'>
+            <div className='flex gap-2'>
                 <AddFriend onAddFriend={onAddFriend} />
                 <Logout/>
             </div>
