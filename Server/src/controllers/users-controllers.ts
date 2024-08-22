2cb5a7d3a8b87510935cd7799a20d139a6c3200f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-08-22 17:38:02 +0800

    🦺 changes in signup for credentials

--- Server/src/controllers/users-controllers.ts
+++ Server/src/controllers/users-controllers.ts
@@ -19,43 +19,38 @@ interface SignupBody{
 }
 
 export const signUp: RequestHandler<unknown, unknown, SignupBody, unknown> = async (req, res, next) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const passwordRaw = req.body.password;
+    const { username, email, password } = req.body;
 
     try {
-        if(!username || !email || !passwordRaw){
-            throw createHttpError(400, "Parameters Missing")
+        if (!username || !email || !password) {
+            throw createHttpError(400, "Parameters Missing");
         }
 
-        const existingUsername = await UserModel.findOne({username: username}).exec();
-
-        if(existingUsername){
-            throw createHttpError(409, "Username already taken, please log in instead.")
+        const existingUsername = await UserModel.findOne({ username }).exec();
+        if (existingUsername) {
+            throw createHttpError(409, "Username already taken, please log in instead.");
         }
 
-        const existingEmail = await UserModel.findOne({email:email}).exec();
-
-        if (existingEmail){
-            throw createHttpError(409, "A user with tis email address already exists. Please log in instead")
+        const existingEmail = await UserModel.findOne({ email }).exec();
+        if (existingEmail) {
+            throw createHttpError(409, "A user with this email address already exists. Please log in instead.");
         }
 
-        const passwordHashed = await bcrypt.hash(passwordRaw, 10)
+        const passwordHashed = await bcrypt.hash(password, 10);
 
         const newUser = await UserModel.create({
-            username: username,
-            email: email,
+            username,
+            email,
             password: passwordHashed,
-        })
+        });
 
         req.session.userId = newUser._id;
-
         res.status(201).json(newUser);
 
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 interface LoginBody{
     username?: string,
